f48534d767f9301b7e6372abb669f1e93a9f30af
MichnoAZ <75491595+MichnoAZ@users.noreply.github.com>
2022-05-01 20:31:38 +0500

    Create CheatWorker.user.js

---
+++ CheatWorker.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name				Cheat Worker by tuba joao pedro
+// @run-at			    document-start
+// @match				https://www.kogama.com/page/webgl-frame/*
+// @match				https://kogama.com.br/page/webgl-frame/*
+// @match				https://friends.kogama.com/page/webgl-frame/*
+// @require             https://raw.githubusercontent.com/SylumTeam-KoGaMa/KoGaMa-Cheat-Worker/main/source.js
+// ==/UserScript==
